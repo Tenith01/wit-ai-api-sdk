a7171df90a2dafb36c4925c5e29adf7e517145a5
Tenith01 <hasintha199@gmail.com>
2025-04-04 21:26:14 +0530

    chore: Init project

---
+++ main.py
@@ -0,0 +1,9 @@
+# main.py
+
+def main():
+    print("Hello, welcome to the simple Python app!")
+    name = input("What's your name? ")
+    print(f"Nice to meet you, {name}!")
+
+if __name__ == "__main__":
+    main()
